9a771bf505c134ecb05964bef10f6eaad1786d1e
ngzh-luke <ngzh-luke@github.com>
2022-11-19 18:53:08 +0700

    initial development

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # The project is developed by Kittipich "Luke" Aiumbhornsin
 # Created on November 19, 2022
+
+from myBalloons import systemInfo, create_app
+
+print("SystemInfo -> ", systemInfo)
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, port=8614)
